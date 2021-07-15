b708437d78f166a18fe868438a9b5ac2d7c196ac
root <root@localhost.localdomain>
2021-07-15 10:43:42 +0100

    Code now makes an HTTP request from bbc

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('Initial testing')
+import requests
+
+request_data = requests.get('https://www.bbc.co.uk/')
+generate_text = request_data.text
+sys_status = request_data.status_code
+print(generate_text,sys_status)
