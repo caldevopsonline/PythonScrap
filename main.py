aaaf29b0dc982be67e35423d53201f55770ea846
root <root@localhost.localdomain>
2021-07-15 12:20:25 +0100

    Script updated to oput .txt file of BBC website content

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-#request_data = requests.get('https://www.bbc.co.uk/')
-#generate_text = request_data.text
-#sys_status = request_data.status_code
-#print(generate_text,sys_status)
+# request_data = requests.get('https://www.bbc.co.uk/')
+# generate_text = request_data.text
+# sys_status = request_data.status_code
+# print(generate_text,sys_status)
 
 
-# Make a request
-page = requests.get(
-    'https://www.bbc.co.uk/')
-soup = BeautifulSoup(page.content, 'html.parser')
+# Make a HTTPS request from the BBC Website
+web_main = requests.get('https://www.bbc.co.uk/')
 
+# Initialize fetch connection with the help og BeautifulSoup Library
+Fetch_data = BeautifulSoup(web_main.content, 'html.parser')
 
-bbc_footer = soup.footer.text
-print(bbc_footer)
+# Get bbc page title
+bbc_title = Fetch_data.title.text
 
+# Get BBC Main Body
+bbc_body = Fetch_data.body.text
 
+# Create a .TXT File
+data_output = open("DATA_FROM_BBC.txt", 'w')
 
+# Push web content to the file created
+data_output.write(bbc_body)
 
+# Close the opened file
+data_output.close()
